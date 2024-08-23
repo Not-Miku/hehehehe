286b055c6480c53f002b01c0e99b43fa11cb41b6
yhmargaret <hanna.alferez24@gmail.com>
2024-08-23 22:13:16 +0800

    added stuff

--- src/pkgtry/Try.java
+++ src/pkgtry/Try.java
@@ -8,14 +8,18 @@ public class Try {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         Random rand = new Random();
-        int min = 1, max = 50, guess;
+        int min = 1, max = 50, max_guess = 5, guess;
         boolean guessed = false;
         
         int random = (int) (rand.nextDouble() * (max - min + 1) + min);
-        System.out.println(random);      
+//        System.out.println(random);    
         
-        for (int i = 0; i < 5; i++) {
-            System.out.print("Enter your guess: ");
+        System.out.println("Guess a Number Between " + min + " and " + max);
+        System.out.println("You Have " + max_guess + " Attempts");
+    
+        for (int i = 0; i < max_guess; i++) {
+            
+            System.out.print("\nEnter your guess: ");
             guess = scan.nextInt();
             
             if (guess == random) {
@@ -27,10 +31,12 @@ public class Try {
             }else{
                 System.out.println("Lower!");
             }
+            System.out.println((max_guess - (i + 1)) + " Attempts Left");
         }
         
         if (!guessed) {
             System.out.println("Game Over");
+            System.out.println("\nThe Number was " + random);
         }
         
     }
